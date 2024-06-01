42de2266fe06e9c1e4232275ff5eb71223eef122
jubairJnu <jubair2810@gmail.com>
2024-06-01 22:45:46 +0600

    initial

---
+++ src/lib/providers/Providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+};
+
+export default Providers;
